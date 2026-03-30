4cb83df8a70aa9794579ac1eb40da6a35538a41f
MUNEZERO Alpha <alphamnzr@gmail.com>
2026-03-30 08:23:34 +0200

    Additional fixes: MongoDB connection options and NODE_ENV in backend start script

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose"
 
-export const connectDB = async() => {
-    try{
-        mongoose.connect(process.env.MONGODB_URI);
+export const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log("MONGODB CONNECTED");
-    }catch(error){
+    } catch (error) {
         console.log(error.message);
         process.exit(1);//exit with failure
     }
